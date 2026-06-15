259f4b20f7ba46ba1ec25bdd5811c133cd8cde55
Anthony Duncalf <anthonyjduncalf@icloud.com>
2026-06-15 18:05:54 +0100

    Add .gitignore, remove node_modules and dist from tracking

--- dist/assets/constants-CbbEX0NZ.js
+++
@@ -1,1 +0,0 @@
-const s="af_heart",o=1,E=50,c=200,n="onnx-community/Kokoro-82M-v1.0-ONNX",t="src/offscreen/offscreen.html",a="src/welcome/welcome.html";export{s as D,c as G,E as M,t as O,a as W,o as a,n as b};
--- dist/service-worker-loader.js
+++
@@ -1,1 +0,0 @@
-import './assets/service-worker.ts-BXO6J-22.js';
